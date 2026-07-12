0bc721a4697163f64bbabb3e68918c34e1c2eda0
Damola Mayowa <damolamayowa43@gmail.com>
2026-07-12 19:23:58 +0100

    Update Supabase configuration in config.js

--- config.js
+++ config.js
@@ -6,3 +6,4 @@ window.ULTRASCORE_CONFIG = {
   SUPABASE_ANON_KEY: "YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY"
 };
 https://tylyxlyrpbasjhlssjxp.supabase.co/rest/v1/
+sb_publishable_gUrwmM1xgM81Wxpqos0NTg_UIv0jJ03
